e50fe1a234f13c8273b9e7700f39cda78457739f
Emmanuel Nkrumah-Sarpong <42615665+nkrumahthis@users.noreply.github.com>
2020-09-16 12:31:45 +0000

    Create Something.java

---
+++ Something.java
@@ -0,0 +1,12 @@
+public class Something {
+  String something;
+  
+  public Something(String something){
+    this.something = something;
+  }
+  
+  public String getSomething(){
+    return this.something;
+  }
+
+}
