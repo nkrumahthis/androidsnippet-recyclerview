1da56ce29cff0dbd290bc2f350905caa54c0a1b0
Emmanuel Nkrumah-Sarpong <42615665+nkrumahthis@users.noreply.github.com>
2020-09-16 12:25:43 +0000

    Create SomethingViewHolder.java

---
+++ SomethingViewHolder.java
@@ -0,0 +1,12 @@
+public class SomethingViewHolder extends ViewHolder {
+
+    public TextView textView;
+
+    public SomethingViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textView = itemView.findViewById(R.id.tvRoomRow);
+
+    }
+
+
+}
